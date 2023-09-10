d7f94c5440e86449606c26d8a3b7a738fe4ea203
Pramo20 <pramodheepa@gmail.com>
2023-09-10 11:46:27 +0530

    local host

--- httpserver.js
+++ httpserver.js
@@ -2,8 +2,16 @@ const express=require('express')
 const app=express()
 const port=3000 
 
-function func(){
-    console.log("Sever is listening on port 3000")
-}
+// function func(){
+//     console.log("Sever is listening on port 3000")
+// }
 
-app.listen(port,func)
+app.listen(port,()=>{
+    console.log("Sever is listening on port 3000")
+})
+
+//var student=[]
+
+app.get('/1',(req,res) => {
+    res.send("Hello World1")
+})
